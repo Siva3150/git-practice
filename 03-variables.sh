8d12df3cd4e77035ee28d70353c690c2825a8494
Siva prasad <sivaprasad3150@gmail.com>
2024-12-30 15:38:15 +0530

    user-profile

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Raheem
+PERSON2=Robert
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $RAHEEM"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
