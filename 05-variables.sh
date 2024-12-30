e937e78cc36363acb13bd568515a3549eed27345
Siva prasad <sivaprasad3150@gmail.com>
2024-12-30 15:47:27 +0530

    user-profile

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "please enter your username::" 
+
+read USERNAME #takes input into USERNAME variable
+
+echo "Username entered is :: $USERNAME"
+
+echo "please enter your password::" 
+
+read PASSWORD
+
+echo "Password entered is::"
+
