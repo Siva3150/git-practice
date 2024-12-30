559a80a1283aba7475b896e16cf2be2e292ab20c
Siva prasad <sivaprasad3150@gmail.com>
2024-12-30 15:54:46 +0530

    user-profile

--- 06-array.sh
+++ 06-array.sh
@@ -3,4 +3,8 @@
 # index starts from 0, size is 3
 FRUITS=("APPLE" "KIWI" "ORANGE")
 
-echo "First fruit is: $FRUITS[0]
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+
+echp "All fruits are: ${FRUITS[@]}
