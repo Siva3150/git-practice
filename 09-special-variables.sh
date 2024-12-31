8a6ceb882e516455ce33695d830826a468098546
Siva prasad <sivaprasad3150@gmail.com>
2024-12-31 18:48:51 +0530

    user-profile

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -4,7 +4,8 @@ echo "All variables passed to the script:: $@"
 
 echo "How many variables/arguments passed to the script:: $#"
 
-echo "script name: $0"
+echo "script name:: $0"
 
 echo "current working directory:: $PWD"
 
+echo "Home directory of current user:: $HOME"
