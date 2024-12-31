9102ccf5e4a5ad42ec3a9e12b561fdd997c9515f
Siva prasad <sivaprasad3150@gmail.com>
2024-12-31 18:39:04 +0530

    user-profile

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -1,3 +1,12 @@
+# #!/bin/bash
+
+# NUMBER1=$1
+# NUMBER2=$2
+
+# SUM=$(($NUMBER1+$NUMBER2))
+
+# echo " sum of $NUMBER1 and $NUMBER2 is $SUM "
+
 #!/bin/bash
 
 NUMBER1=$1
@@ -5,5 +14,5 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2))
 
-echo " sum of $NUMBER1 and $NUMBER2 is $SUM "
+echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
 
