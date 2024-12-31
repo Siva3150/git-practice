983ab6427755605938d6cf37113fec10b3027e46
Siva prasad <sivaprasad3150@gmail.com>
2024-12-31 19:33:54 +0530

    user-profile

---
+++ 12-functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+Check_root() {
+    if [ $USERID -ne 0 ]
+    then
+    echo "please run this script with root previleges"
+    exit 1
+fi
+
+}
+
+check_root 
+
+dnf list installed mysql # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is not installed, going to install it"
+    dnf install mysql -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql installation is not success. check it please"
+        exit 1
+    else
+        echo " Mysql installation is successful"
+    fi
+
+else
+echo "mysql is already installed nothing to do ..."
+fi
