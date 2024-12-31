c7da35753adddafe0e5364064eb154b289e4b507
Siva prasad <sivaprasad3150@gmail.com>
2024-12-31 19:17:50 +0530

    user-profile

--- 11-install-git.sh
+++ 11-install-git.sh
@@ -12,6 +12,15 @@ dnf list installed mysql
 if [ $? -ne 0 ]
 then
 echo "mysql is not installed, going to install it"
+dnf install mysql -y
+
+if [$? -ne 0 ]
+then
+echo "Mysql installation is not success"
+else
+echo " Mysql installation is successful"
+fi
+
 else
 echo "mysql is already installed nothing to do ..."
 fi
