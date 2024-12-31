c1f671b99957a444127517a0d86871001bb495e6
Siva prasad <sivaprasad3150@gmail.com>
2024-12-31 19:23:19 +0530

    user-profile

--- 11-install-git.sh
+++ 11-install-git.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 USERID=$(id -u)
+#echo "User ID is: $USERID"
 
 if [ $USERID -ne 0 ]
 then
-echo "please run this script with root previleges"
+    echo "please run this script with root previleges"
 fi
 
-dnf list installed mysql
+dnf list installed mysql # Just checking whether installed or not
 
 if [ $? -ne 0 ]
 then
-echo "mysql is not installed, going to install it"
-dnf install mysql -y
-
-if [ $? -ne 0 ]
-then
-echo "Mysql installation is not success"
-else
-echo " Mysql installation is successful"
-fi
+    echo "mysql is not installed, going to install it"
+    dnf install mysql -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Mysql installation is not success"
+    else
+        echo " Mysql installation is successful"
+    fi
 
 else
 echo "mysql is already installed nothing to do ..."
