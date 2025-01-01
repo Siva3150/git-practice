d4e37e9e6ece40f3164871051b29c86dac0a05a4
Siva prasad <sivaprasad3150@gmail.com>
2025-01-01 19:08:58 +0530

    user-profile

---
+++ 14-loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {0..10} #0-10
+do
+  echo $I
+done
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+# echo "7"
+# echo "8"
+# echo "9"
+# echo "10"
