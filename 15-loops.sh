c38027a9e1db85557788f27e97d281a567d15bcb
Siva prasad <sivaprasad3150@gmail.com>
2025-01-01 19:30:42 +0530

    user-profile

--- 15-loops.sh
+++ 15-loops.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
-for package in $@ 
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+check_root() {
+    if [ $USERID -ne 0 ]
+    then
+    echo  "please run this script with root previleges"
+    exit 1
+fi
+
+}
+
+validate() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is... $R failure $N"
+    else
+        echo -e "$2 is... $Y success... $N"
+    fi
+}
+
+check_root
+
+# sh 15-loops.sh git mysql postfix nginx
+
+for package in $@  # $@ refers to all arguments passed to it
 do
- echo $package
+  dnf list installed $package
+  if [ $? -ne 0 ]
+  then 
+      echo "installing $package is not installed, going to install it"
+      dnf install $package -y
+      validate $? "Installing $package"
+  else
+      echo "$package is already installed nothing to do ..."
+  fi
+
 done
+
+
