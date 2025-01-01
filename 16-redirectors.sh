731ced9dceb7936a95433bf3350d9daf6e21aa8e
Siva prasad <sivaprasad3150@gmail.com>
2025-01-01 20:41:18 +0530

    user-profile

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -49,11 +49,11 @@ fi
 
 for package in $@  # $@ refers to all arguments passed to it
 do
-  dnf list installed $package &>>$LOG_FILE
+  dnf list installed $package &>> $LOG_FILE
   if [ $? -ne 0 ]
   then 
       echo "installing $package is not installed, going to install it" | tee -a $LOGFILE
-      dnf install $package -y &>>$LOG_FILE
+      dnf install $package -y &>> $LOG_FILE
       validate $? "Installing $package"
   else
       echo -E "$package is already $Y installed nothing to do ... $N" | tee -a $LOGFILE
