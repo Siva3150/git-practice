50c6d0d8915de68cefad2b6abc053e5184d9ba51
Siva prasad <sivaprasad3150@gmail.com>
2025-01-20 16:45:25 +0530

    user-profile

---
+++ 17-set.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e # setting the automatic exit, if we get error, set -ex for debug
+
+echo "Hello World success"
+echooooo "Hello Wolrd failure"
+echo "Hello World after failure"
